efc67b8f48b258dd9a1ab6060607122d6a01bb4d
jeandrei <jeandreiwalter@gmail.com>
2021-09-23 12:52:54 +0000

    aula 411

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const path = require('path');
 //Require no mongoose
 const mongoose = require('mongoose');
+//method override necessário para poder usar metodos como DELETE, PUT ?_method=DELETE
+//tem que instalar npm i method-override --silent
+const methodOverride = require('method-override');
 //Require campground lá da pasta models que demos um export
 const Campground = require('./models/campground');
 
@@ -30,16 +33,57 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+//parse de request body necessário para enviar os dados pelo post req.body aula 410
+app.use(express.urlencoded({extended: true}))
+//string que será usada para identificar o method override ?_method=DELETE
+app.use(methodOverride('_method'));
+
 
 app.get('/', (req,res) => {
     res.render('home');
 })
 
-app.get('/makecampground', async (req,res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'cheap caming!'});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req,res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
 })
+
+//ADD NEW LOAD THE FORM
+//nessa rota a ordem importa o /campgrounds/new tem que vim antes 
+//do /campgrounds/:id caso contrário ele vai tratar o new como um id
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+//SAVE THE DATA SENT FROM THE FORM aula 410
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+//SHOW
+app.get('/campgrounds/:id', async (req,res) => { 
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+
+//EDIT
+//Get data to show in the form
+app.get('/campgrounds/:id/edit', async (req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+//aula 411
+app.put('/campgrounds/:id', async (req,res) => {
+    //res.send("IT WORKED!!!");
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{ ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
