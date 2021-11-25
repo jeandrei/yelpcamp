3b6a711ef7f7fe840f036586acd1ddf2f90f9fcc
jeandrei <jeandreiwalter@gmail.com>
2021-11-25 13:19:00 +0000

    add start script

--- app.js
+++ app.js
@@ -280,8 +280,12 @@ app.use((err,req, res, next) => {
     res.status(statusCode).render('error', { err });   
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
+
+//process.env.PORT vem do heroku automaticamente
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
 })
 
 
